ab2c0e6901ba889d5a266ab8c4e29508225bd999
Fatima Rodriguez <ac.frodriguez@login01.af.uchicago.edu>
2025-05-03 21:42:58 -0500

    added

--- GNN/training.py
+++ GNN/training.py
@@ -31,10 +31,8 @@ for i in range(10,20):
     features = np.hstack((hits[['x','y','z']]/1000, hit_cells.reshape(len(hit_cells),1)/10,hit_value.reshape(len(hit_cells),1)))
     all_node_features.append(features)
 
-    # edges
     particle_ids = truth.particle_id.unique()
     particle_ids = particle_ids[np.where(particle_ids!=0)[0]]
-
     for particle_id in particle_ids:
         hit_ids = truth[truth.particle_id == particle_id].hit_id.values-1
         for i in hit_ids:
@@ -50,12 +48,15 @@ for i in range(10,20):
                     if rj < ri:
                         all_source_nodes.append(j)
                         all_target_nodes.append(i)
-                        delta = features[i,:3] - features[j:3]
+                        delta = features[i,:3] - features[j,:3]
 
                     # getting edge features
                     all_deltas.append(delta)
                     all_distances.append(np.linalg.norm(delta))
 
+all_pos_edges = list(zip(all_source_nodes, all_target_nodes))
+#print(all_pos_edges)
+#print(len(all_pos_edges))
 
 # generating hard negatives
 from sklearn.neighbors import NearestNeighbors
@@ -74,26 +75,27 @@ def generate_hard_negatives(features_all, pos_edges, k=5):
 
 # Combine features from all events
 features_all = np.vstack([event_features for event_features in all_node_features])
-hard_neg_edges = generate_hard_negatives(features_all, all_pos_edges, k=5)
-# Subsample negatives
+all_neg_edges = generate_hard_negatives(features_all, all_pos_edges, k=20)
 num_pos = len(all_pos_edges)
-hard_neg_edges = np.random.choice(hard_neg_edges, size=num_pos, replace=False)
+num_neg = len(all_neg_edges)
+print("num positives:", num_pos)
+print("num negatives:", num_neg)
 all_neg_deltas = []
 all_neg_distances = []
-for u, v in hard_neg_edges:
+for u, v in all_neg_edges:
     delta = features_all[v, :3] - features_all[u, :3]
     all_neg_deltas.append(delta)
     all_neg_distances.append(np.linalg.norm(delta))
 
 # combining
 # edge indices
-edge_index = np.array(all_pos_edges + hard_neg_edges).T 
+edges = np.array(all_pos_edges + all_neg_edges) 
 # edge labels
-edge_y = np.concatenate([np.ones(num_pos), np.zeros(num_pos)])
+labels = np.concatenate([np.ones(num_pos), np.zeros(num_neg)])
 # edge features
-edge_attr = np.vstack([
-    np.hstack([all_pos_deltas, np.array(all_pos_distances).reshape(-1, 1)]),  # Positives
-    np.hstack([all_neg_deltas, np.array(all_neg_distances).reshape(-1, 1)])   # Negatives
+edge_features = np.vstack([
+    np.hstack([all_deltas, np.array(all_distances).reshape(-1, 1)]),
+    np.hstack([all_neg_deltas, np.array(all_neg_distances).reshape(-1, 1)])
 ])
 
 # need to shuffle in unison
@@ -104,9 +106,9 @@ edge_features = edge_features[shuffle_idx]
 
 data = Data(
     x=torch.tensor(features_all, dtype=torch.float),
-    edge_index=torch.tensor(edge_index, dtype=torch.long),
-    edge_attr=torch.tensor(edge_attr, dtype=torch.float),
-    edge_y=torch.tensor(edge_y, dtype=torch.float)
+    edge_index=torch.tensor(edges.T, dtype=torch.long),
+    edge_attr=torch.tensor(edge_features, dtype=torch.float),
+    edge_y=torch.tensor(labels, dtype=torch.float)
 )
 
 ## GNN MODEL
@@ -136,10 +138,10 @@ class EdgeClassifier(torch.nn.Module):
 
 # training loop 
 model = EdgeClassifier(node_feat_dim=5, edge_feat_dim=4, hidden_dim=64)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 criterion = torch.nn.BCELoss()
 
-def train(data):
+def train(model, data):
     model.train()
     optimizer.zero_grad()
     out = model(data)
@@ -148,9 +150,36 @@ def train(data):
     optimizer.step()
     return loss.item()
 
-for epoch in range(30):
-    loss = train(data)
-    print(f"Epoch {epoch}, Loss: {loss:.4f}")
+# going to add validation check and split data 80/20
+from torch_geometric.transforms import RandomLinkSplit
+edge_transform = RandomLinkSplit(num_val = .2, num_test = .8, is_undirected=False, split_labels=True)
+train_data, val_data, test_data = edge_transform(data)
 
-torch.save(model.state_dict(), 'gcn_model.pth')
+def validate(model, data):
+    model.eval()
+    with torch.no_grad():
+        out = model(data)
+        val_loss = criterion(out, data.edge_y)
+    return val_loss
+
+patience = 10
+epochs_no_improve = 0
+best_val_loss = float('inf')
+for epoch in range(100):
+    loss = train(model, train_data)
+    print(f"Epoch {epoch}, Loss: {loss:.4f}")
+    total_norm = torch.sqrt(sum(p.grad.pow(2).sum() for p in model.parameters()))
+    print(f"Gradient norm: {total_norm:.6f}")
+    val_loss = validate(model, val_data)
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        epochs_no_improve = 0 
+        torch.save(model.state_dict(), '/data/ac.frodriguez/best_gnn_model.pth')
+    else:
+        epochs_no_improve += 1
+        if epochs_no_improve >= patience:
+            print("early stopping!")
+            break
+
+torch.save(model.state_dict(), 'final_gcn_model.pth')
 
